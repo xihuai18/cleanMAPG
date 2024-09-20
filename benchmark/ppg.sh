2ff0a0a1ab7e12ce7c679ab37bd97a670cf42c97
xihuai18 <wang_98xh@qq.com>
2024-09-21 01:31:00 +0800

    chore: :fire: remove unnecessary files

--- benchmark/ppg.sh
+++
@@ -1,8 +0,0 @@
-# export WANDB_ENTITY=openrlbenchmark
-
-poetry install -E procgen
-xvfb-run -a poetry run python -m cleanrl_utils.benchmark \
-    --env-ids starpilot bossfight bigfish \
-    --command "poetry run python cleanrl/ppg_procgen.py --track --capture_video" \
-    --num-seeds 3 \
-    --workers 1
